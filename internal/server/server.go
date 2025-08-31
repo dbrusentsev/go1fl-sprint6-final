a7bd2c6500090bb8ba5062e44b9b31f9b02234b4
Dmitry Brusentsev <dbrusentsev@ptsecurity.com>
2025-08-31 17:30:24 +0300

    feat: add service

--- internal/server/server.go
+++ internal/server/server.go
@@ -1,1 +1,39 @@
 package server
+
+import (
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/Yandex-Practicum/go1fl-sprint6-final/internal/handlers"
+)
+
+type Server struct {
+	logger *log.Logger
+	server *http.Server
+}
+
+func NewServer(logger *log.Logger) *Server {
+	mux := http.NewServeMux()
+	
+	mux.HandleFunc("/", handlers.IndexHandler)
+	mux.HandleFunc("/upload", handlers.UploadHandler)
+	
+	server := &http.Server{
+		Addr:         ":8080",
+		Handler:      mux,
+		ErrorLog:     logger,
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 10 * time.Second,
+		IdleTimeout:  15 * time.Second,
+	}
+	
+	return &Server{
+		logger: logger,
+		server: server,
+	}
+}
+
+func (s *Server) Start() error {
+	return s.server.ListenAndServe()
+}
